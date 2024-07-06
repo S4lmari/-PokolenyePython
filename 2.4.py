5a9601ae509686fdc424a188d79ada405eef0164
Salmari <tommysalmari@gmail.com>
2024-07-06 17:58:52 +0300

    Целочисленная арифметика

---
+++ 2.4.py
@@ -0,0 +1,50 @@
+# x = int(input())
+# y = x + 1
+# z = y + 1
+# print(x, y, z, sep=("\n"))
+
+# x = int(input())
+# y = int(input())
+# z = int(input())
+# print(x + y + z)
+
+# l = int(input())
+# v = l**3
+# s = 6 * (l**2)
+# print('Объем =', v)
+# print('Площадь полной поверхности =', s)
+
+# a = int(input())
+# b = int(input())
+# x = (3 * (a + b)**3) + 275 * b**2 - 127 * a - 41
+# print(x)
+
+# x = int(input())
+# y = x - 1
+# z = x + 1
+# print('Следу.щее за числом', x,'число:', z)
+# print('Для числа', x, 'предыдущее число:', y)
+
+# m = int(input())
+# s = int(input())
+# k = int(input())
+# mouse = int(input())
+# summ = (m + s + k + mouse) * 3
+# print(summ)
+
+# a = int(input())
+# b = int(input())
+# print(str(a),'+',str(b),'=', a + b)
+# print(str(a),'-',str(b),'=', a - b)
+# print(str(a),'*',str(b),'=', a * b)
+
+# a1 = int(input())
+# d = int(input())
+# n = int(input())
+
+# an = a1 + d*(n-1)
+
+# print(an)
+
+x = int(input())
+print(x, 2 * x, 3 * x, 4 * x, 5 * x, sep=("---"))
