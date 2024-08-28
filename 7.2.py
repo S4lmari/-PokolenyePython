33c134a59f953468edc6a25a225a8435c63fa0b1
Salmari <tommysalmari@gmail.com>
2024-08-28 09:36:08 +0300

    Цикл for функция range

--- 7.2.py
+++ 7.2.py
@@ -4,13 +4,45 @@
 #     print(i)
 # print(n)
 
-m, n = int(input()), int(input())
+# m, n = int(input()), int(input())
+#
+# if m >= n:
+#     for i in range(m, n, -1):
+#         print(i)
+#     print(n)
+# else:
+#     for i in range(m, n):
+#         print(i)
+#     print(n)
 
-if m >= n:
-    for i in range(m, n, -1):
-        print(i)
-    print(n)
-else:
-    for i in range(m, n):
-        print(i)
-    print(n)
+# m, n = int(input()), int(input())
+#
+# if (m-n)> 1:
+#     for i in range(m, n, -1):
+#         if i % 2 != 0:
+#             print(i)
+#     if n % 2 != 0:
+#         print(n)
+#
+# elif abs(m - n) == 1:
+#     if m % 2 != 0:
+#         print(m)
+#     else:
+#         print(n)
+
+
+# m, n = int(input()), int(input())
+#
+#
+# for i in range(m, n):
+#     if i % 17 == 0 or i % 10 == 9 or (i % 3 == 0 and i % 5 == 0):
+#         print(i)
+#
+# if n % 17 == 0 or n % 10 == 9 or (n % 3 == 0 and n % 5 == 0):
+#     print(n)
+
+
+n = int(input())
+
+for i in range(10):
+    print(n,'x', i+1, '=',n*(i+1))
