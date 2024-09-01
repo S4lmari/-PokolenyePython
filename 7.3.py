5ff8ae66fa2b1a0632040854c2f8594a1ef06491
Salmari <tommysalmari@gmail.com>
2024-09-01 19:46:53 +0300

    Цикл for функция range

--- 7.3.py
+++ 7.3.py
@@ -33,4 +33,53 @@
 # print(expression)
 
 
+# n = int(input())
+# summ = 0
+# for i in range(1,n+1):
+#     if (i**2 % 10) == 2 or (i**2 % 10) == 5 or (i**2 % 10) == 8:
+#         summ += i
+#
+# print(summ)
 
+# n = int(input())
+# x = 1
+#
+# for i in range(1,n+1):
+#     x *= i
+# print(x)
+
+# summ = 1
+# for i in range(10):
+#     x = int(input())
+#     if x != 0:
+#         summ *= x
+# print(summ)
+
+# n = int(input())
+# summ = 0
+# for i in range(1,n+1):
+#     if (n%i) == 0:
+#         summ += i
+# print(summ)
+
+# n = int(input())
+# summ = 0
+# for i in range(1, n + 1):
+#     summ += ((-1) ** (i + 1)) * i
+# print(summ)
+
+n = int(input())
+largest  = 0
+largest_2 = 0
+
+for i in range(n):
+    x = int(input())
+
+    if x > largest:
+        largest = x
+
+    if x < largest and x > largest_2:
+        largest_2 = x
+
+
+print(largest, largest_2, sep="\n")
