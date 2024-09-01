689ac0e62f7557e33a25888f45b3e5f9aa36012c
Salmari <tommysalmari@gmail.com>
2024-09-01 18:07:55 +0300

    Цикл for функция range

---
+++ 7.3.py
@@ -0,0 +1,36 @@
+# total = 0
+# for i in range(1, 6):
+#     total += i
+#     print(total, end='')
+
+# a, b = int(input()), int(input())
+#
+# cube = 0
+#
+# for i in range(a, b+1):
+#     if (i ** 3) % 10 == 4 or (i ** 3) % 10 == 9:
+#         cube += 1
+# print(cube)
+
+
+# n = int(input())
+# sum = 0
+#
+# for _ in range(n):
+#     x = int(input())
+#     sum += x
+#
+# print(sum)
+
+# import math
+#
+# n = int(input())
+# x = 0
+# for i in range(1,n+1):
+#     x += (1/i)
+#     expression = x - math.log(n)
+#
+# print(expression)
+
+
+
