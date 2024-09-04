b3c7341e96ac48703fc43ddff5d202b0856c0c03
Salmari <tommysalmari@gmail.com>
2024-09-04 22:54:09 +0300

    цикл while

--- 7.4.py
+++ 7.4.py
@@ -1,8 +1,35 @@
+# text = " "
+# while text != 'КОНЕЦ' and text != 'конец':
+#     print(text)
+#     text = input()
 
-text = " "
-while text != ('КОНЕЦ' or 'конец'):
 
-    if text == ('КОНЕЦ' or 'конец'):
+# text = ''
+# count = 0
+#
+# while text != 'стоп' and text != 'достаточно' and text != 'хватит':
+#     text = input()
+#     if text != 'стоп' and text != 'достаточно' and text != 'хватит':
+#         count += 1
+# print(count)
 
-    print(text)
-    text = input()
+# num = int(input())
+#
+# while num % 7 == 0:
+#     print(num)
+#     num = int(input())
+
+# num = int(input())
+# sum = 0
+# while num >= 0:
+#     sum += num
+#     num = int(input())
+# print(sum)
+
+num = int(input())
+sum = 0
+while 1 <= num <= 5:
+    if num == 5:
+        sum += 1
+    num = int(input())
+print(sum)
