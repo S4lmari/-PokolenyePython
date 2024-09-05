6afdfef4850f735f6341b2cc76d511e13dfecf01
Salmari <tommysalmari@gmail.com>
2024-09-05 07:45:27 +0300

    цикл while

--- 7.4.py
+++ 7.4.py
@@ -26,10 +26,31 @@
 #     num = int(input())
 # print(sum)
 
-num = int(input())
-sum = 0
-while 1 <= num <= 5:
-    if num == 5:
-        sum += 1
-    num = int(input())
-print(sum)
+# num = int(input())
+# sum = 0
+# while 1 <= num <= 5:
+#     if num == 5:
+#         sum += 1
+#     num = int(input())
+# print(sum)
+
+
+pay = int(input())
+count = 0
+while pay >= 25:
+    count += 1
+    pay -= 25
+
+while 25 > pay >= 10:
+    count += 1
+    pay -= 10
+
+while 10 > pay >= 5:
+    count += 1
+    pay -= 5
+
+while 5 > pay >= 1:
+    count += 1
+    pay -= 1
+
+print(count)
