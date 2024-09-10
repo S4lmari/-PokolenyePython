540854f07c46b65fb1f43ebab51f6ac8fbab0e2e
Salmari <tommysalmari@gmail.com>
2024-09-10 09:17:12 +0300

    Вложенные циклы

---
+++ 7.8.py
@@ -0,0 +1,21 @@
+# counter = 0
+# for i in range(99, 102):
+#     temp = i
+#     while temp > 0:
+#         counter += 1
+#         temp //= 10
+# print(counter)
+
+# n = int(input())
+#
+# for _ in range(n):
+#     for _ in range(3):
+#         print(n, end=(" "))
+#     print()
+
+n = int(input())
+
+for i in range(1,n+1):
+    for j in range(1,6):
+        print(i,end=(' '))
+    print()
