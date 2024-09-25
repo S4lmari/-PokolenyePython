030b8926dacde2b5b213c11a73c0bcd505d63a22
S4lmari <tommysalmari@gmail.com>
2024-09-25 10:30:08 +0300

    Вложеные циклы тест

--- 7.8.py
+++ 7.8.py
@@ -13,13 +13,45 @@
 #         print(n, end=(" "))
 #     print()
 
-n = int(input())
+# n = int(input())
+#
+# for i in range(1,n+1):
+#     print("")
+#     for j in range(1,10):
+#         summ = i + j
+#         print(i,'+',j,'=',summ)
 
-for i in range(1,n+1):
-    for j in range(1,6):
-        print(i,end=(' '))
-    print()
+# n = int(input())
+# middle =  n//2
+#
+# for i in range(1,middle+1):
+#     print(i*"*")
+#
+#
+# for i in range(middle+1,0,-1):
+#     print(i*"*")
 
+# n =int(input())
+#
+# for i in range(1,n+1):
+#     print(str(i)*i)
 
-# new
+# total = 0
+# for b in range(1, 10):
+#     for k in range(1, 20):
+#         for t in range(1, 200):
+#             if 10*b + 5*k + 0.5*t == 100 and b+k+t == 100:
+#                 total += 1
+#                 print('b =', b, 'k =', k, 't =', t)
+# print('Общее количество натуральных решений =', total)
+
+for e in range(151):
+    print(e)
+    for a in range(1, e):
+        for b in range(a, e):
+            for c in range(b, e):
+                for d in range(c, e):
+                    if a ** 5 + b ** 5 + c ** 5 + d ** 5 == e ** 5:
+                        summ = a + b + c + d + e
+                        print(a, b, c, d, e, summ)
 
