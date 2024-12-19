4cc18e9e14b82deee3564e2fa576dce0e1f639aa
S4lmari <tommysalmari@gmail.com>
2024-12-19 09:19:33 +0300

    Вложеные циклы ч1

--- 7.9.py
+++ 7.9.py
@@ -1,3 +1,5 @@
+from math import factorial
+
 from unicodedata import numeric
 
 # n = int(input())
@@ -26,24 +28,68 @@ from unicodedata import numeric
 #             print(k,end='')
 
 
+# a = int(input())
+# b = int(input())
+#
+# maxsum = 0
+# maxnumber = 0
+#
+# for i in range(a,b+1):
+#     sum = 0
+#
+#     for j in range(1,i+1):
+#         if i % j == 0:
+#             sum += j
+#
+#     if sum >= maxsum:
+#         maxsum = sum
+#         maxnumber = i
+#
+# print(maxnumber,maxsum)
+
+# n = int(input())
+# count = 0
+#
+# for i in range(1, n + 1):
+#     count = 0
+#     for x in range(1, i + 1):
+#         if i % x == 0:
+#             count += 1
+#     print(i, + count * '+', sep=(''))
+
+# n = int(input())
+# last = 0
+#
+# while n > 9:
+#     summ = 0
+#     while n > 0:
+#         last = n % 10
+#         summ += last
+#         n //= 10
+#     n = summ
+# print(n)
+
+# import math
+# n = int(input())
+# total = 0
+#
+# for i in range(1, n + 1):
+#
+#     for x in range(1, i + 1):
+#         f = factorial(x)
+#     total += f
+#
+# print(total)
+
 a = int(input())
 b = int(input())
 
-maxsum = 0
-maxnumber = 0
+for i in range(a, b + 1):
+    count = 0
 
-for i in range(a,b+1):
-    sum = 0
-
-    for j in range(1,i+1):
-        if i % j == 0:
-            sum += j
-
-    if sum >= maxsum:
-        maxsum = sum
-        maxnumber = i
-
-print(maxnumber,maxsum)
+    for x in range(1, i + 1):
+        if i % x == 0:
+            count += 1
 
 
 
