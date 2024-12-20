371ba36cac87a4c2ca58dc1db57abf56f86d9cd4
S4lmari <tommysalmari@gmail.com>
2024-12-20 07:29:22 +0300

    Вложеные циклы ч1

--- 7.9.py
+++ 7.9.py
@@ -85,11 +85,14 @@ a = int(input())
 b = int(input())
 
 for i in range(a, b + 1):
+
     count = 0
 
     for x in range(1, i + 1):
         if i % x == 0:
             count += 1
+    if count == 2:
+        print(i)
 
 
 
