2e4a7fda850cdab6d162c9ef04a42ea495e810a7
Alex <tommysalmari@gmail.com>
2024-12-22 18:23:06 +0300

    Строковый тип данных (Срезы)

--- 9.2.py
+++ 9.2.py
@@ -9,6 +9,20 @@
 #
 # print(len(s),s*3,s[0],s[:3],s[-3:],s[::-1],s[1:len(s)],sep="\n")
 
+# s = input()
+#
+# print(s[2],s[-2],s[:5],s[:len(s)-2],s[::2],s[1::2],s[::-1],s[::-2],sep='\n')
+
 s = input()
 
-print(s[2],s[-2],s[:5],s[:len(s)-2],s[::2],s[1::2],s[::-1],s[::-2],sep='\n')
+if len(s) % 2 != 0:
+   first = len(s)//2 +1
+   left = s[:first]
+   right = s[first:]
+   print(right+left)
+else:
+    mid = len(s)//2
+    left = s[:mid]
+    right = s[mid:]
+    print(right+left)
+
