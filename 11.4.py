0b996820607247c2d8c93d78b0e344fbd5209d85
Alex <tommysalmari@gmail.com>
2025-01-01 14:15:00 +0300

    Вывод элементов списка

--- 11.4.py
+++ 11.4.py
@@ -3,14 +3,64 @@
 # for num in numbers:
 #     summ += num ** 2
 # print(summ)
+# n = int(input())
+# list = []
+#
+# for i in range(n):
+#     list.append(int(input()))
+# for el in list:
+#     print(el)
+# print('')
+# for j in list:
+#     x = j ** 2 + 2 * j + 1
+#     print(x)
+# n = int(input())
+# list = []
+# for el in range(n):
+#     list.append(int(input()))
+#
+# list.remove(max(list))
+# list.remove(min(list))
+#
+# for j in list:
+#     print(j)
+from itertools import count
+
+# n = int(input())
+# list = []
+# for i in range(n):
+#     st = input()
+#     if st in list:
+#         continue
+#     else:
+#         list.append(st)
+#
+# for j in list:
+#     print(j)
+# n = int(input())
+# list = []
+# for i in range(n):
+#     list.append(input())
+# promt = input()
+# for el in list:
+#     if promt.lower() in el.lower():
+#         print(el)
 n = int(input())
 list = []
 
 for i in range(n):
-    list.append(int(input()))
+    list.append(input())
+
+k = int(input())
+list_request = []
+
+for j in range(k):
+    list_request.append(input())
+
 for el in list:
-    print(el)
-print('')
-for j in list:
-    x = j ** 2 + 2 * j + 1
-    print(x)
+    count = 0
+    for r in list_request:
+        if r.lower() in el.lower():
+            count += 1
+        if count == k:
+            print(el)
