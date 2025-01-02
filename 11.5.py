20b97e0389b54384dc9450b790e159dfa72ef775
Alex <tommysalmari@gmail.com>
2025-01-02 12:48:00 +0300

    Методы строк

---
+++ 11.5.py
@@ -0,0 +1,52 @@
+# s = 'BEEGEEK'
+# chars = list(s)
+# s = '**'.join(chars)
+# print(s)
+from itertools import count
+
+# st = input()
+# text = st.split()
+#
+# print("\n".join(text))
+
+# st = input().split()
+# for i in st:
+#     print(i[0],end='.')
+
+# st =input().split('\\')
+# for el in st:
+#     print(el)
+
+# numders = input().split()
+# for el in numders:
+#     print(int(el) * "+")
+
+# ip = input().split('.')
+# flag = None
+# for el in ip:
+#     if int(el) not in range(255):
+#         flag = False
+#         break
+#     else:
+#         flag = True
+# if flag == True:
+#     print("ДА")
+# else:
+#     print("НЕТ")
+
+# st = input()
+# separate = input()
+#
+# print(separate.join(st))
+
+nums = input().split()
+count = 0
+
+for num in nums:
+    if nums.count(num) == 2:
+        count += 0.5
+    if nums.count(num) >=2:
+        nums.count(num)*()
+
+
+print(count)
