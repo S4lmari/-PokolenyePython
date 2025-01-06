9e190faa21cfa31134af27eea948813da6f3b205
S4lmari <tommysalmari@gmail.com>
2025-01-06 19:08:05 +0300

    Списочные выражения

--- 11.7.py
+++ 11.7.py
@@ -14,6 +14,20 @@
 #
 # print(lengths)
 
-palindromes = [i for i in range(100, 1000) if i // 100 == i % 10]
+# palindromes = [i for i in range(100, 1000) if i // 100 == i % 10]
+#
+# print(palindromes)
 
-print(palindromes)
+# n = int(input())
+#
+# square = [print(i**2) for i in range(1,n+1)]
+
+# str = input().split()
+#
+# kube = [print(int(el)**3,end=" ") for el in str]
+
+# one_str = [print(el) for el in input().split()]
+
+# one_str_2 = [print(e, end='') for el in input().split() for e in el if e in "0123456789"]
+
+square = [print(int(num) ** 2,end=' ') for num in input().split() if int(num) % 2 == 0 and (int(num) ** 2) % 10 != 4]
